056e743f6c19ca86a69c01a5c5dc73d5a3122c3f
Joshua Bassett <hello@joshbassett.info>
2025-04-12 20:37:04 +1000

    Fix wall collision handling

--- multy.c
+++ multy.c
@@ -13,28 +13,28 @@ static void update_cell(grid_t *next_grid, const grid_t *prev_grid, size_t x,
                         size_t y) {
   if (prev_grid->cells[y][x] & CELL_UP) {
     if (y == 0) {
-      next_grid->cells[y][x] |= CELL_DOWN;
+      next_grid->cells[y + 1][x] |= CELL_DOWN;
     } else {
       next_grid->cells[y - 1][x] |= CELL_UP;
     }
   }
   if (prev_grid->cells[y][x] & CELL_DOWN) {
     if (y == GRID_SIZE - 1) {
-      next_grid->cells[y][x] |= CELL_UP;
+      next_grid->cells[y - 1][x] |= CELL_UP;
     } else {
       next_grid->cells[y + 1][x] |= CELL_DOWN;
     }
   }
   if (prev_grid->cells[y][x] & CELL_LEFT) {
     if (x == 0) {
-      next_grid->cells[y][x] |= CELL_RIGHT;
+      next_grid->cells[y][x + 1] |= CELL_RIGHT;
     } else {
       next_grid->cells[y][x - 1] |= CELL_LEFT;
     }
   }
   if (prev_grid->cells[y][x] & CELL_RIGHT) {
     if (x == GRID_SIZE - 1) {
-      next_grid->cells[y][x] |= CELL_LEFT;
+      next_grid->cells[y][x - 1] |= CELL_LEFT;
     } else {
       next_grid->cells[y][x + 1] |= CELL_RIGHT;
     }
